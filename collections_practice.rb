ea2bea4511665153e890d98628e062e4019b57d9
Jennifer Dirmeyer <dirmeyer@gmail.com>
2019-04-15 21:22:21 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -21,6 +21,7 @@ end
 
 def swap_elements_from_to(array, index, destination_index)
   array[destination_index], array[index] = array[index], array[destination_index]
+  array
 end  
 
 def reverse_array(array)
